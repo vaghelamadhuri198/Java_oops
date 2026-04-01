7cfbd6c2fdef0b4451b6aa195338258c5c509fe5
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-01 17:55:51 +0530

    Super construction.java

---
+++ Super construction.java
@@ -0,0 +1,16 @@
+class A {
+	A() {
+
+		System.out.println("i am madhuri");
+	}
+}
+class B extends A {
+	B() {
+         super();
+		System.out.println("i am madhvi");
+	}
+	public static void main(String[]args) {
+		B a1 = new B();
+
+	}
+}
