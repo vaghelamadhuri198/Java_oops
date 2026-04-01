b235c8cfb1760d4d866edfa20ac4587196f2375c
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-01 18:04:25 +0530

    Static block.java
    🔹 Definition
    A static block is a block of code inside a class that runs automatically before the main() method and only once.

---
+++ Static block.java
@@ -0,0 +1,10 @@
+class Example {
+    static {
+        // code to be executed
+        System.out.println("Static block executed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method executed");
+    }
+}
